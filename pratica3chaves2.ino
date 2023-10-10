61e39b0df4b170eda4b76e1888692a3c302180ce
GustavopRezende <130861649+GustavopRezende@users.noreply.github.com>
2023-10-10 10:37:44 -0300

    Update pratica3chaves2.ino

--- pratica3chaves2.ino
+++ pratica3chaves2.ino
@@ -1,27 +1,33 @@
 #define I0 2 // define a macro relativa a entrada (pino) 2 - em cada pino um LED
 #define I1 3
 #define I2 4
-#define S0 5 // define a macro relativa a saida (pino) 5 - em cada pino um LED
-#define S1 6
-#define S2 7
+#define S1 8
+#define S2 9
 
+int converteDecimal(int b0, int b1, int b2) {
+   int decimal = 0;
+   decimal = b0 + b1 * 2 + b2 * 4; // Conversão pela notação polinomial
+   return decimal; // Retorna o valor calculado para o codigo que chama a funcao
+}
 
 void setup(){
-   pinMode(I0,INPUT); // Configura os pinos I0, I1 e I2 como entrada
-   pinMode(I1,INPUT);
-   pinMode(I2,INPUT);
-   pinMode(S0,OUTPUT); // Configura os pinos S0, S1 e S2 como saida
-   pinMode(S1,OUTPUT);
-   pinMode(S2,OUTPUT);
+   pinMode(I0,OUTPUT);  // Configura os pinos I0, I1 e I2 como entrada
+   pinMode(I1,OUTPUT);
+   pinMode(I2,OUTPUT);
+   pinMode(S1,INPUT);
+   pinMode(S2,INPUT);
+   Serial.begin(9600); // Abre a porta serial, define a taxa de dados de comunicação serial para 9600 bps
 }
 
 void loop(){
-   int a, b , c; // Declara as variáveis internas a funcao loop 
+   int a, b, c, saida; // Declara as variáveis internas a funcao loop 
    a = digitalRead(I0); // Le a entrada e atribui o valor lido a variavel
    b = digitalRead(I1);
    c = digitalRead(I2);
-   digitalWrite(S0,!a); // Inverte o valor lógico lido na entrada e acende a respectiva saída
-   digitalWrite(S1,!b);
-   digitalWrite(S2,!c);
-   delay(5000); // Proporciona um atraso de 5000 milissegundos entre dois loops 
+   digitalWrite(I0,!a); // Inverte o valor lógico lido na entrada e acende a respectiva saída
+   digitalWrite(I1,!b);
+   digitalWrite(I2,!c);
+   saida = converteDecimal(a,b,c); // A variavel saida recebe o valor da funcao converteDecimal
+   Serial.println(saida,DEC);  // Escreve o valor armazenado na variavel na saida serial
+   delay(3000); // Pausa o programa em 3000 milissegundos
 }
